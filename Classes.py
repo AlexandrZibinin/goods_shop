802d14d433e02ab862038248d8cd3f4dd5f21294
Alexandr Zibinin <zibinin.chel@gmail.com>
2024-02-25 22:18:45 +0300

    небольшой ремонт после лайва

--- Classes.py
+++ Classes.py
@@ -17,11 +17,6 @@ class Category:
     def goods(self):
         return self.__goods
 
-    # @goods.getter
-    # def get_goods(self):
-    #     """вывод информации о товаре"""
-    #     for value in self.__goods:
-    #         print(f"{value[0]}, {value[2]} руб. Остаток: {value[3]} шт.")
 
     @goods.setter
     def goods(self, new_goods):
@@ -33,39 +28,27 @@ class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
         self.total_categories = 0
         self.total_unique_products = 0
 
     @classmethod
     def create_product(cls, product_data: dict):
+        """создание экземпляра класса продукта"""
         name = product_data['name']
         description = product_data['description']
         price = product_data['price']
         quantity = product_data['quantity']
         return cls(name, description, price, quantity)
 
+    @property
+    def price(self):
+        return self.__price
 
-    # def new_product(cls, new_product):
-    #     """Для класса Product добавить метод, который создает товар и возвращает объект,
-    #     который можно добавлять в список товаров."""
-    #     name, description, price, quantity = new_product.split(' ')
-    #     return cls(name, description, price, quantity)
-    #
-    #
-    #
-    # def get_price(self):
-    #     return self.price
-    #
-    #
-    # def get_price(self, new_price):
-    #     self.price = new_price
-    #
-    # def get_info_price(self):
-    #     """если цена ниже или равна 0 выводит сообщение о некорректной цене"""
-    #     if self.get_price() <= 0:
-    #         print('цена введена некорректная')
-    #
-    # def create_product(self):
-    #     return [self.name, self.__price, self.quantity]
+    @price.setter
+    def check_price(self, new_price) -> float:
+        if new_price <= 0:
+            return 'Цена введена некорректная'
+        self.__price = new_price
+
--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from Classes import Category, Product
-
-my_cat = Category('fruit', 'round', [Product('груша', 'красная', 110.0, 5)])
-# вот продукт
-product_data = {
-        "name": "Samsung Galaxy C23 Ultra",
-        "description": "256GB, Серый цвет, 200MP камера",
-        "price": 180000.0,
-        "quantity": 5
-      }
-# пытаемся добавить продукт в категорию
-my_new = Product.create_product(product_data)
-
-my_cat.goods = my_new
-
-print(my_cat)
+# from Classes import Category, Product
+#
+# my_cat = Category('fruit', 'round', [Product('груша', 'красная', 110.0, 5)])
+# # вот продукт
+# product_data = {
+#         "name": "Samsung Galaxy C23 Ultra",
+#         "description": "256GB, Серый цвет, 200MP камера",
+#         "price": 180000.0,
+#         "quantity": 5
+#       }
+# # пытаемся добавить продукт в категорию
+# my_new = Product.create_product(product_data)
+#
+# print(my_new.price)
+#
+#
+# my_cat.goods = my_new
+#
+# my_new.check_price()
+#
+# print(my_cat)
 
 
 # my_cat.get_goods()
